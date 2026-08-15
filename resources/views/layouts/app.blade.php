7919f1ca52060f5370e679bc69a4149a4f06481c
lapkin <magangrsudbangil@gmail.com>
2026-08-15 10:19:41 +0700

    hapus register

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Web Kesehatan BNSP</title>
+    <title>Web Kesehatan</title>
     <!-- Bootstrap 5 CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Chart.js -->
